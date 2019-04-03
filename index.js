35898df88baeae30ebe07f3c98c835b831803f7d
ahmedkhalaf1996 <khalafahmed1996@gmail.com>
2019-04-02 21:41:05 -0700

    enh

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 require('./startup/logging')();
 require('./startup/routes')(app);
-require('./startup/db')();
+//require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
@@ -13,3 +13,5 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
+
+return "hellow worled"
