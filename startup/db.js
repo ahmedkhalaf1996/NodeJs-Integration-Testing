d4475cbf5aff050b63fab7fda5c7a1fb1c9cbed4
ahmedkhalaf1996 <khalafahmed1996@gmail.com>
2019-04-02 20:10:46 -0700

    ad1

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  const db = 'mongodb+srv://Ahmed:1234@cluster0-vozhb.mongodb.net/test?retryWrites=true';
+  const db = "mongodb+srv://Ahmed:1234@cluster0-vozhb.mongodb.net/test?retryWrites=true";
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
 }
